b513eda885c8a644b55752d153fc03f04c9e5ecc
Fezzer <chris.bowles@thescreamingsheep.net>
2023-02-27 22:38:56 +0000

    add question prompt and initial save of html file

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-import Manager from "./lib/Manager";
-import Engineer from "./lib/Engineer";
-import Intern from "./lib/Intern";
+import Manager from "./lib/Manager.js";
+import Engineer from "./lib/Engineer.js";
+import Intern from "./lib/Intern.js";
 import inquirer from "inquirer";
-import { resolve, join } from "path";
-import fs from "fs";
-
-const OUTPUT_DIR = resolve(__dirname, "output");
-const outputPath = join(OUTPUT_DIR, "team.html");
-
 import render from "./src/page-template.js";
+import { writeFile } from "node:fs/promises";
+
+const addEngineer = "Add a new engineer";
+const addIntern = "Add a new intern";
+const finish = "Finish building your team";
+const employees = [];
+
+const nextQuestion = [{
+  type: "list",
+  name: "next",
+  message: "What do you want to do next?",
+  choices: [addEngineer, addIntern, finish]
+}];
+
+async function init() {
+  const managerQuestions = Manager.generateQuestions();
+  const managerAnswers = await inquirer.prompt(managerQuestions);
+  employees.push(Manager.fromAnswers(managerAnswers));
+
+  let finished = false;
+
+  while (true) {
+    const nextAnswer = await inquirer.prompt(nextQuestion);
+    let questions;
+    let fromAnswers;
+
+    switch (nextAnswer.next) {
+      case addEngineer:
+          questions = Engineer.generateQuestions();
+          fromAnswers = Engineer.fromAnswers;
+        break;
+    
+      case addIntern:
+          questions = Intern.generateQuestions();
+          fromAnswers = Intern.fromAnswers;
+        break;
+    
+      default:
+        finished = true;
+        break;
+    }
+
+    if (finished)
+      break;
+
+    const answers = await inquirer.prompt(questions);
+    employees.push(fromAnswers(answers));
+  }
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+  const html = render(employees);
+  await writeFile("team.html", html, { encoding: "utf8" });
+}
+
+init();
